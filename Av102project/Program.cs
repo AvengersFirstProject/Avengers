758f91fe139f4f74519330d2411ae5d0814fc69a
satnam998 <satnam.singh@lngconsultancy.co.in>
2021-02-11 16:13:52 +0530

    yessss

--- Av102project/Program.cs
+++ Av102project/Program.cs
@@ -14,7 +14,7 @@ namespace Av102project
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            Console.WriteLine("hello world how are you");
+            Console.WriteLine("hello world how are youddddddd");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
