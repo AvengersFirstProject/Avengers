9eef7c2fa79367f7ac426b76b40aec01260a4cc5
sahilsharma6375 <sahilsharma6375@gmail.com>
2021-02-17 15:27:27 +0530

    commit made

---
+++ ApiFlixLngAvengers/ApiFlixlngAvengers.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiFlixlngAvengers.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ ApiFlixLngAvengers/ApiFlixlngAvengers.Service/Interface/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiFlixlngAvengers.Service.Interface
+{
+    public interface ICustomerService
+    {
+        IActionResult SaveUser(CustomerRequest request);
+    }
+}
